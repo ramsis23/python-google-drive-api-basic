939389c9d31be73c62ae5a9daf423f0aae085415
Rahmat Siswanto <rahmatsiswanto23@gmail.com>
2019-03-03 23:05:17 +0700

    Update testUpload.py

--- testUpload.py
+++ testUpload.py
@@ -1,12 +1,10 @@
 from __future__ import print_function
 import pickle
 import os.path
-import httplib2
 from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
 from googleapiclient.http import MediaFileUpload, MediaIoBaseDownload
-import auth
 
 # If modifying these scopes, delete the file token.pickle.
 SCOPES = ['https://www.googleapis.com/auth/drive']
